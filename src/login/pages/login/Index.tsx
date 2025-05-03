565187796965d1ffc5e672b343020738a35fd095
lcaohoanq <hoangclw@gmail.com>
2025-05-03 16:43:12 +0700

    style: re-style login page

--- src/login/pages/login/Index.tsx
+++ src/login/pages/login/Index.tsx
@@ -1,5 +1,17 @@
 import { PageProps } from "../../types";
-import { Box, Button, CardContent, Divider, Grid2 as Grid, Link, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CardContent,
+  Divider,
+  Grid2 as Grid,
+  Link,
+  TextField,
+  Typography,
+  Stack,
+  useTheme,
+  Paper,
+} from "@mui/material";
 import { HintBox } from "../../../components/HintBox";
 import { useState } from "react";
 import { LoadingButton } from "@mui/lab";
@@ -8,50 +20,171 @@ const Login = (props: PageProps<"login.ftl">) => {
   const [loading, setLoading] = useState(false);
   const { i18n, Template, kcContext } = props;
   const { url, realm, social, message, messagesPerField } = kcContext;
-  const { loginWithEmailAllowed, resetPasswordAllowed, registrationAllowed } = realm
+  const { loginWithEmailAllowed, resetPasswordAllowed, registrationAllowed } =
+    realm;
   const { msgStr } = i18n;
+  const theme = useTheme();
 
   return (
     <Template i18n={i18n} kcContext={kcContext}>
-      <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-        <Box marginBottom="20px" textAlign="center">
-          <Typography variant="h4">{msgStr('doLogIn')}</Typography>
-        </Box>
-        <form onSubmit={() => setLoading(true)} id="kc-form-login" action={url.loginAction} method="post">
-          <Grid container spacing={2}>
-            <Grid size={12}>
-              <TextField error={messagesPerField.existsError('username')} helperText={messagesPerField.getFirstError('username')}  fullWidth label={loginWithEmailAllowed ? msgStr('usernameOrEmail') : msgStr('username')} name="username" id="username"></TextField>
-            </Grid>
-            <Grid size={12}>
-              <TextField error={messagesPerField.existsError('password')} fullWidth type="password" label={msgStr('password')} name="password" id="password"></TextField>
-            </Grid>
-            <Grid display="flex" flexDirection="column" size={12}>
-              <LoadingButton fullWidth variant="contained" loading={loading} type="submit">{msgStr('doLogIn')}</LoadingButton>
-              {message && <HintBox style={{ marginTop: "10px", textAlign: "center" }} type={message?.type === 'success' ? 'info' : message.type} message={message.summary} />}
-            </Grid>
-          </Grid>
-          {(resetPasswordAllowed || registrationAllowed || !!social?.providers?.length) && (
-            <Box textAlign="center">
-              <Divider sx={{ margin: "20px 0px" }}></Divider>
-              {!!social?.providers?.length && (
-                social.providers.map((x, i) => <Button id={`social-${x.alias}`} href={x.loginUrl} sx={{ marginBottom: "10px" }} key={i} variant="outlined" fullWidth>{x.displayName}</Button>)
-              )}
-              {registrationAllowed && (
-                <Box display="flex" justifyContent="center" alignItems="center" columnGap="5px">
-                  <Typography variant="caption">{msgStr('noAccountYet')}</Typography>
-                  <Typography variant="caption" href={url.registrationUrl} component={Link}>{msgStr('doRegister')}</Typography>
-                </Box>
-              )}
-              {resetPasswordAllowed && (
-                <Box display="flex" justifyContent="center" alignItems="center" columnGap="5px">
-                  <Typography variant="caption">{msgStr('doForgotPassword')}</Typography>
-                  <Typography variant="caption" href={url.loginResetCredentialsUrl} component={Link}>{msgStr('resetPassword')}</Typography>
-                </Box>
-              )}
-            </Box>
-          )}
-        </form>
-      </CardContent>
+      <Paper elevation={4}>
+        <CardContent sx={{ p: 4 }}>
+          <Box mb={3} textAlign="center">
+            <Typography variant="h4" fontWeight="medium" color="primary">
+              {/* {msgStr("doLogIn")} */}
+              {"Shoppe"}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" mt={1}>
+              {msgStr("loginAccountTitle", "Sign in to your account")}
+            </Typography>
+          </Box>
+
+          <form
+            onSubmit={() => setLoading(true)}
+            id="kc-form-login"
+            action={url.loginAction}
+            method="post"
+          >
+            <Stack spacing={2.5} mb={3}>
+              <TextField
+                error={messagesPerField.existsError("username")}
+                helperText={messagesPerField.getFirstError("username")}
+                fullWidth
+                label={
+                  loginWithEmailAllowed
+                    ? msgStr("usernameOrEmail")
+                    : msgStr("username")
+                }
+                name="username"
+                id="username"
+                variant="outlined"
+                size="medium"
+                autoComplete="username"
+                autoFocus
+              />
+
+              <TextField
+                error={messagesPerField.existsError("password")}
+                helperText={messagesPerField.getFirstError("password")}
+                fullWidth
+                type="password"
+                label={msgStr("password")}
+                name="password"
+                id="password"
+                variant="outlined"
+                size="medium"
+                autoComplete="current-password"
+              />
+
+              <LoadingButton
+                fullWidth
+                variant="contained"
+                loading={loading}
+                type="submit"
+                size="large"
+                sx={{
+                  mt: 1,
+                  py: 1.2,
+                  fontWeight: "medium",
+                }}
+              >
+                {msgStr("doLogIn")}
+              </LoadingButton>
+            </Stack>
+
+            {message && (
+              <HintBox
+                style={{ marginBottom: theme.spacing(2) }}
+                type={message?.type === "success" ? "info" : message.type}
+                message={message.summary}
+              />
+            )}
+
+            {/* Account links */}
+            {(resetPasswordAllowed || registrationAllowed) && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                mb={2}
+              >
+                {resetPasswordAllowed && (
+                  <Typography
+                    variant="body2"
+                    component={Link}
+                    href={url.loginResetCredentialsUrl}
+                    underline="hover"
+                  >
+                    {msgStr("doForgotPassword")}
+                  </Typography>
+                )}
+                {registrationAllowed && (
+                  <Typography
+                    variant="body2"
+                    component={Link}
+                    href={url.registrationUrl}
+                    underline="hover"
+                  >
+                    {msgStr("doRegister")}
+                  </Typography>
+                )}
+              </Box>
+            )}
+
+            {/* Social providers */}
+            {!!social?.providers?.length && (
+              <>
+                <Divider sx={{ my: 3 }}>
+                  <Typography variant="body2" color="text.secondary" px={1}>
+                    {msgStr(
+                      "identity-provider-login-label",
+                      "Or continue with"
+                    )}
+                  </Typography>
+                </Divider>
+
+                <Grid container spacing={2}>
+                  {social.providers.map((provider, i) => (
+                    <Grid
+                      size={(social.providers ?? []).length > 3 ? 6 : 12}
+                      key={i}
+                    >
+                      <Button
+                        id={`social-${provider.alias}`}
+                        href={provider.loginUrl}
+                        variant="outlined"
+                        fullWidth
+                        size="large"
+                        sx={{
+                          borderRadius: 1,
+                          textTransform: "none",
+                          justifyContent: "flex-start",
+                          py: 1,
+                        }}
+                      >
+                        <Box
+                          component="span"
+                          sx={{
+                            width: 24,
+                            height: 24,
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            mr: 1,
+                          }}
+                        >
+                          <i className={provider.iconClasses}></i>
+                        </Box>
+                        {provider.displayName}
+                      </Button>
+                    </Grid>
+                  ))}
+                </Grid>
+              </>
+            )}
+          </form>
+        </CardContent>
+      </Paper>
     </Template>
   );
 };
---
+++ src/register/Index.tsx
@@ -0,0 +1,355 @@
+import { useState } from "react";
+import {
+  Box,
+  CardContent,
+  TextField,
+  Typography,
+  Stack,
+  Link,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  FormHelperText,
+  Grid,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { HintBox } from "../../../components/HintBox";
+import { PageProps } from "../../types";
+
+const Register = (props: PageProps<"register.ftl">) => {
+  const [loading, setLoading] = useState(false);
+  const { i18n, Template, kcContext } = props;
+  const {
+    url,
+    message,
+    messagesPerField,
+    profile,
+    recaptchaRequired,
+    recaptchaSiteKey,
+    termsAcceptanceRequired,
+  } = kcContext;
+  const { msgStr } = i18n;
+
+  // Parse profile attributes
+  const { attributesByName = {} } = profile || {};
+  const attributes = Object.entries(attributesByName || {});
+
+  return (
+    <Template i18n={i18n} kcContext={kcContext}>
+      <Paper elevation={4}>
+        <CardContent sx={{ p: 4 }}>
+          <Box mb={3} textAlign="center">
+            <Typography variant="h4" fontWeight="medium" color="primary">
+              {msgStr("registerTitle", "Create Account")}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" mt={1}>
+              {msgStr("registerAccountTitle", "Register a new account")}
+            </Typography>
+          </Box>
+
+          <form
+            onSubmit={() => setLoading(true)}
+            id="kc-register-form"
+            action={url.registrationAction}
+            method="post"
+          >
+            <Stack spacing={2.5} mb={3}>
+              {/* Username field (if not using email as username) */}
+              {attributesByName.username !== undefined && (
+                <TextField
+                  id="username"
+                  name="username"
+                  label={msgStr("username")}
+                  defaultValue={attributesByName.username?.value || ""}
+                  error={messagesPerField.existsError("username")}
+                  helperText={messagesPerField.get("username")}
+                  fullWidth
+                  autoComplete="username"
+                  autoFocus
+                  disabled={attributesByName.username?.readOnly === true}
+                  required
+                />
+              )}
+
+              {/* Email field */}
+              {attributesByName.email !== undefined && (
+                <TextField
+                  id="email"
+                  name="email"
+                  type="email"
+                  label={msgStr("email")}
+                  defaultValue={attributesByName.email?.value || ""}
+                  error={messagesPerField.existsError("email")}
+                  helperText={
+                    messagesPerField.get("email") ||
+                    attributesByName.email?.annotations?.inputHelperTextBefore
+                  }
+                  fullWidth
+                  autoComplete="email"
+                  disabled={attributesByName.email?.readOnly === true}
+                  required
+                />
+              )}
+
+              {/* First name field */}
+              {attributesByName.firstName !== undefined && (
+                <TextField
+                  id="firstName"
+                  name="firstName"
+                  label={msgStr("firstName")}
+                  defaultValue={attributesByName.firstName?.value || ""}
+                  error={messagesPerField.existsError("firstName")}
+                  helperText={messagesPerField.get("firstName")}
+                  fullWidth
+                  autoComplete="given-name"
+                  disabled={attributesByName.firstName?.readOnly === true}
+                  required={attributesByName.firstName?.required !== false}
+                />
+              )}
+
+              {/* Last name field */}
+              {attributesByName.lastName !== undefined && (
+                <TextField
+                  id="lastName"
+                  name="lastName"
+                  label={msgStr("lastName")}
+                  defaultValue={attributesByName.lastName?.value || ""}
+                  error={messagesPerField.existsError("lastName")}
+                  helperText={messagesPerField.get("lastName")}
+                  fullWidth
+                  autoComplete="family-name"
+                  disabled={attributesByName.lastName?.readOnly === true}
+                  required={attributesByName.lastName?.required !== false}
+                />
+              )}
+
+              {/* Password fields */}
+              <TextField
+                id="password"
+                name="password"
+                type="password"
+                label={msgStr("password")}
+                error={messagesPerField.existsError("password")}
+                helperText={messagesPerField.get("password")}
+                fullWidth
+                autoComplete="new-password"
+                required
+              />
+
+              <TextField
+                id="password-confirm"
+                name="password-confirm"
+                type="password"
+                label={msgStr("passwordConfirm")}
+                error={messagesPerField.existsError("password-confirm")}
+                helperText={messagesPerField.get("password-confirm")}
+                fullWidth
+                autoComplete="new-password"
+                required
+              />
+
+              {/* Custom attributes rendering */}
+              {attributes
+                .filter(
+                  ([name]) =>
+                    !["username", "email", "firstName", "lastName"].includes(
+                      name
+                    )
+                )
+                .map(([name, attribute]) => {
+                  // Skip if attribute is undefined
+                  if (!attribute) return null;
+
+                  const {
+                    displayName,
+                    required = true,
+                    readOnly = false,
+                    annotations = {},
+                    validators = {},
+                    value = "",
+                  } = attribute;
+
+                  // Handle options type attributes (dropdown select)
+                  if (validators.options?.options) {
+                    const options = validators.options.options;
+                    const optionLabelsPrefix =
+                      annotations.inputOptionLabelsI18nPrefix;
+
+                    return (
+                      <FormControl
+                        key={name}
+                        fullWidth
+                        error={messagesPerField.existsError(name)}
+                        disabled={readOnly}
+                      >
+                        <InputLabel id={`${name}-label`}>
+                          {displayName}
+                        </InputLabel>
+                        <Select
+                          labelId={`${name}-label`}
+                          id={name}
+                          name={name}
+                          defaultValue={value || ""}
+                          label={displayName}
+                          required={required}
+                        >
+                          {options.map((option) => (
+                            <MenuItem key={option} value={option}>
+                              {optionLabelsPrefix
+                                ? msgStr(
+                                    `${optionLabelsPrefix}.${option}`,
+                                    option
+                                  )
+                                : option}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                        {messagesPerField.existsError(name) && (
+                          <FormHelperText>
+                            {messagesPerField.get(name)}
+                          </FormHelperText>
+                        )}
+                      </FormControl>
+                    );
+                  }
+
+                  // Handle checkbox type attributes
+                  if (annotations.inputType === "multiselect-checkboxes") {
+                    const options = validators.options?.options || [];
+                    const optionLabels = annotations.inputOptionLabels || {};
+
+                    return (
+                      <Box key={name}>
+                        <Typography variant="subtitle2" mb={1}>
+                          {displayName}
+                        </Typography>
+                        {options.map((option) => (
+                          <FormControlLabel
+                            key={option}
+                            control={
+                              <Checkbox
+                                name={`${name}[${option}]`}
+                                value={option}
+                                disabled={readOnly}
+                                defaultChecked={value === option}
+                              />
+                            }
+                            label={optionLabels[option] || option}
+                          />
+                        ))}
+                        {messagesPerField.existsError(name) && (
+                          <FormHelperText error>
+                            {messagesPerField.get(name)}
+                          </FormHelperText>
+                        )}
+                      </Box>
+                    );
+                  }
+
+                  // Default to text input
+                  return (
+                    <TextField
+                      key={name}
+                      id={name}
+                      name={name}
+                      label={displayName}
+                      defaultValue={value || ""}
+                      error={messagesPerField.existsError(name)}
+                      helperText={
+                        messagesPerField.get(name) ||
+                        annotations.inputHelperTextBefore
+                      }
+                      fullWidth
+                      disabled={readOnly}
+                      required={required}
+                    />
+                  );
+                })}
+
+              {/* Terms acceptance */}
+              {termsAcceptanceRequired && (
+                <Box>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        name="termsAccepted"
+                        id="termsAccepted"
+                        required
+                      />
+                    }
+                    label={
+                      <Typography variant="body2">
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html:
+                              msgStr("termsText") ||
+                              "I agree to the Terms of Service",
+                          }}
+                        />
+                      </Typography>
+                    }
+                  />
+                  {messagesPerField.existsError("termsAccepted") && (
+                    <FormHelperText error>
+                      {messagesPerField.get("termsAccepted")}
+                    </FormHelperText>
+                  )}
+                </Box>
+              )}
+
+              {/* reCAPTCHA */}
+              {recaptchaRequired && (
+                <Box my={2} display="flex" justifyContent="center">
+                  <div
+                    className="g-recaptcha"
+                    data-sitekey={recaptchaSiteKey}
+                  ></div>
+                </Box>
+              )}
+
+              {/* Submit button */}
+              <LoadingButton
+                fullWidth
+                variant="contained"
+                loading={loading}
+                type="submit"
+                size="large"
+                sx={{
+                  mt: 1,
+                  py: 1.2,
+                  fontWeight: "medium",
+                }}
+              >
+                {msgStr("doRegister")}
+              </LoadingButton>
+            </Stack>
+
+            {message && (
+              <HintBox
+                style={{ marginBottom: 16 }}
+                type={message?.type === "success" ? "info" : message.type}
+                message={message.summary}
+              />
+            )}
+
+            {/* Login link */}
+            <Box textAlign="center" mt={2}>
+              <Typography variant="body2">
+                {msgStr("alreadyHaveAccount", "Already have an account?")}{" "}
+                <Link href={url.loginUrl} underline="hover">
+                  {msgStr("doLogIn")}
+                </Link>
+              </Typography>
+            </Box>
+          </form>
+        </CardContent>
+      </Paper>
+    </Template>
+  );
+};
+
+export { Register };
